d665ce7c0ef8f27f44b1c95275f53dbe4bd0149e
ThePaulper <pnboris2020@gmail.com>
2024-05-15 19:39:35 -0600

    Created Pancake Practice

---
+++ Basics/001 Conventions and Syntax/BorisPLab1Practice.cs
@@ -0,0 +1,29 @@
+using System;
+
+
+  public class PancakeProgram
+  {
+    public Pancake PaulCake;
+
+    public void Main()
+    {
+      PaulCake = new Pancake()  
+      public int extra = 5;
+      int extra += 5
+      PaulCake.centimeters++;
+      public double total = pancakes + extra;
+      public double pancakes = 37.5D;
+      public string yum = "Pancakes are delicious!";
+      public bool trueBool = true;
+      Console.WriteLine(name + "thinks" + yum);
+      Console.WriteLine("Paul made" + total + "pancakes.");
+     }
+  }
+
+  public class Pancake
+  {
+    public int centimeters = 32;
+      inches = 22;
+    public string cakeName = "Paul's Pancake";
+    public float yumminess = 99.99f;
+  }
